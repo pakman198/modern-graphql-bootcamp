9e0032e4002efcd065b223f90ffd3dcb65fc69a2
Pako Herrera <pakman_198@hotmail.com>
2021-04-21 16:23:55 -0700

    Installed missing deps and graphql-yoga to have a playground. Created a dummy schema and resolvers

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,28 @@
-console.log("Hello GraphQL")
+import { GraphQLServer } from 'graphql-yoga';
+
+// Type definitions (schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+  }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    hello: () => {
+      return 'This is my first Query!';
+    },
+    name: () => 'Pako Herrera'
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => {
+  console.log('🚀 The server us up')
+});
