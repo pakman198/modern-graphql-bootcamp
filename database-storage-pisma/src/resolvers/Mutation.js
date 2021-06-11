f5f2046dc21b21678542680837258537a96685d8
Pako Herrera <pakman_198@hotmail.com>
2021-06-11 10:20:38 -0700

    Added a new Post model to prisma schema. Updated the GraphQL schema to add the new Post type and add CRUD operations to it

--- database-storage-pisma/src/resolvers/Mutation.js
+++ database-storage-pisma/src/resolvers/Mutation.js
@@ -1,7 +1,7 @@
 const Mutation = {
-  async createUser(parent, args, { prisma }, info) {
+  async createUser(parent, args, ctx, info) {
     const { name, email } = args.data;
-    const newUser = await prisma.user.create({
+    const newUser = await ctx.prisma.user.create({
       data: {
         name,
         email
@@ -10,8 +10,8 @@ const Mutation = {
   
     return newUser;
   },
-  async updateUser(parent, args, { prisma }, info) {
-    const { id, name, email } = args.data;
+  async updateUser(parent, { data }, { prisma }, info) {
+    const { id, name, email } = data;
     const updatedUser = await prisma.user.update({
       where: { id },
       data: { name, email },
@@ -19,14 +19,80 @@ const Mutation = {
 
     return updatedUser;
   },
-  async deleteUser(parent, args, { prisma }, info) {
-    const { userId } = args;
+  async deleteUser(parent, { userId }, { prisma }, info) {
+    // at this moment prisma 2 doesnt support cascade delete so we need to delete relations manually
+    const posts = await prisma.post.deleteMany({
+      where: { userId }
+    });
+
     const user = await prisma.user.delete({
       where: { id: userId },
     })
 
     return user;
-  }
+  },
+  async createPost(parent, { data }, { prisma }, info) {
+    const { author, title, body, published } = data;
+    const userExists = await prisma.user.findUnique({
+      where: { id: author }
+    });
+
+    if(!userExists) {
+      throw new Error('User not found');
+    }
+
+    const post = await prisma.post.create({
+      data: {
+        title,
+        body,
+        published: !!published,
+        author: {
+          connect: { id: author }
+        }
+      },
+      include: {
+        author: true
+      }
+    });
+
+    console.log({ post })
+
+    return post;
+  },
+  async updatePost(parent, { postId, data }, { prisma }, info) {
+    
+    // This is not necessary, but it gives you a better idea of why the query will fail
+    // If i didn't add this validation, the update query will just display some prisma error
+    // that will be hard to present to the user
+    const postExists = await prisma.post.findUnique({
+      where: { id: postId }
+    });
+
+    if(!postExists) {
+      throw new Error('Post not found');
+    }
+
+    const post = await prisma.post.update({
+      where: {
+        id: postId
+      },
+      data: {...data},
+      include: {
+        author: true
+      }
+    });
+
+    console.log({ post });
+
+    return post;
+  },
+  async deletePost(parent, { postId }, { prisma }, info) {
+    const post = await prisma.post.delete({
+      where: { id: postId },
+    })
+
+    return post;
+  },
 }
 
 export default Mutation;
--- database-storage-pisma/src/resolvers/Query.js
+++ database-storage-pisma/src/resolvers/Query.js
@@ -10,15 +10,48 @@ const Query = {
     const user = await prisma.user.findUnique({
       where: {
         id: userId
-      }
+      },
+      include: { posts: true }
     });
+
+    if(!user) {
+      throw new Error('User not found')
+    }
 
     return user;
   },
   async users(parent, args, { prisma }, info) {
-    const users = await prisma.user.findMany();
+    const users = await prisma.user.findMany({
+      include: { posts: true }
+    });
 
     return users;
+  },
+  async post(parent, args, { prisma }, info) {
+    const { postId } = args;
+    const post = await prisma.post.findUnique({
+      where: {
+        id: postId
+      },
+      include: {
+        author: true
+      }
+    });
+
+    if(!post) {
+      throw new Error('Post not found')
+    }
+
+    return post;
+  },
+  async posts(parent, args, { prisma }, info) {
+    const posts = await prisma.post.findMany({
+      include: {
+        author: true
+      }
+    });
+
+    return posts;
   }
 }
 
